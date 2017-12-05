a651769bd86095811603844507a4900c3b0cc53a
Without Boats <boats@mozilla.com>
2017-12-04 19:37:58 -0800

    Add is_default and is_overriden methods.

--- configure/src/source.rs
+++ configure/src/source.rs
@@ -12,20 +12,21 @@
 //! Libraries should **never** try to set the configuration source; only
 //! binaries should ever override the default.
 use std::sync::{Once, ONCE_INIT};
+use std::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
 
 use serde::Deserializer;
 use erased_serde::Deserializer as DynamicDeserializer;
 
 pub use default::DefaultSource;
 
+
 /// The global static holding the active configuration source for this project.
 pub static CONFIGURATION: ActiveConfiguration = ActiveConfiguration {
-    _private: (),
+    init: ONCE_INIT,
+    is_overriden: ATOMIC_BOOL_INIT,
 };
 
 static mut SOURCE: Option<&'static (Fn(&'static str) -> Box<DynamicDeserializer> + Send + Sync + 'static)> = None;
-
-static INIT: Once = ONCE_INIT;
 
 /// The active configuration source.
 ///
@@ -34,7 +35,8 @@ static INIT: Once = ONCE_INIT;
 /// the configuration source using the `set` method, while libraries which
 /// need to be configured can use the `get` method.
 pub struct ActiveConfiguration {
-    _private: (),
+    init: Once,
+    is_overriden: AtomicBool,
 }
 
 impl ActiveConfiguration {
@@ -73,7 +75,8 @@ impl ActiveConfiguration {
         F: Fn(&'static str) -> D + Send + Sync + 'static,
         D: for<'de> Deserializer<'de> + 'static,
     {
-        INIT.call_once(||  {
+        self.init.call_once(||  {
+            self.is_overriden.store(true, Ordering::Relaxed);
             let init = Box::new(move |s| {
                 let deserializer = initializer(s);
                 Box::new(DynamicDeserializer::erase(deserializer)) as Box<DynamicDeserializer>
@@ -88,11 +91,25 @@ impl ActiveConfiguration {
     /// the active source of configuration. Normally they would derive
     /// Configure for their config struct, which will call this method.
     pub fn get(&'static self, package: &'static str) -> Box<DynamicDeserializer> {
-        INIT.call_once(|| {
+        self.init.call_once(|| {
             let source = DefaultSource::init();
             let init = Box::new(move |s| source.prepare(s));
             unsafe { SOURCE = Some(&*Box::into_raw(init)) }
         });
         unsafe { SOURCE.unwrap()(package) }
+    }
+
+    /// Returns true if the configuration source is the default source.
+    ///
+    /// The opposite of `CONFIGURATION.is_default()`
+    pub fn is_default(&'static self) -> bool {
+        !self.is_overriden()
+    }
+
+    /// Returns true if the configuration source has been overriden.
+    ///
+    /// The opposite of `CONFIGURATION.is_default()`
+    pub fn is_overriden(&'static self) -> bool {
+        self.is_overriden.load(Ordering::Relaxed)
     }
 }
